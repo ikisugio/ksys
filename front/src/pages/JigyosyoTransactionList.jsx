83245234a2b8b6beba730f874d4b9831434f7a85
ikisugio <tkmst.work@gmail.com>
2023-12-25 17:34:17 +0900

    1225v2

--- front/src/pages/JigyosyoTransactionList.jsx
+++ front/src/pages/JigyosyoTransactionList.jsx
@@ -1,5 +1,47 @@
+import { Typography, Timeline, TimelineItem, TimelineSeparator, TimelineDot, TimelineConnector, TimelineContent, Card, CardContent } from '@mui/material';
+
+
 const JigyosyoSearch = () => {
-  return <div>Transaction List</div>;
-};
+  // ダミーデータの例
+  const items = [
+    {
+      date: "2023-01-01",
+      title: "イベント1",
+      description: "イベント1の説明文"
+    },
+    {
+      date: "2023-02-01",
+      title: "イベント2",
+      description: "イベント2の説明文"
+    },
+    {
+      date: "2023-03-01",
+      title: "イベント3",
+      description: "イベント3の説明文"
+    }
+  ];
+
+  return (
+    <Timeline>
+      {items.map((item, index) => (
+        <TimelineItem key={index}>
+          <TimelineSeparator>
+            <TimelineDot />
+            {index < items.length - 1 && <TimelineConnector />}
+          </TimelineSeparator>
+          <TimelineContent>
+            <Card sx={{ mb: 2 }}>
+              <CardContent>
+                <Typography variant="h6">{item.title}</Typography>
+                <Typography>{item.description}</Typography>
+                <Typography color="textSecondary">{item.date}</Typography>
+              </CardContent>
+            </Card>
+          </TimelineContent>
+        </TimelineItem>
+      ))}
+    </Timeline>
+  );
+}
 
 export default JigyosyoSearch;
